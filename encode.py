11b55b62265ff167e0cfe84b2d8f1f7283038542
saru <sarumathiayyappan@gmail.com>
2019-06-07 09:52:39 +0530

    Create encode.py

---
+++ encode.py
@@ -0,0 +1,11 @@
+string1=input()
+string2=input()
+string3=''
+for i in range(0,len(string1)):
+    number1=(ord(string1[i]))-96
+    number2=(ord(string2[i]))-96
+    if((number1+number2)<=26):
+        string3=string3+(chr(96+number1+number2))
+    else:
+        string3=string3+(chr(96+(number1+number2-26)))
+print(string3)        
